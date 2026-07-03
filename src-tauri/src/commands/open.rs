eb57f2c1d3aeca1666cc8c3a99ccbab2869452c2
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2026-07-03 19:20:04 +0200

    feat(i18n): create translation folder when opening it from the app

--- src-tauri/src/commands/open.rs
+++ src-tauri/src/commands/open.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::path::{Path, PathBuf};
 
 use anyhow::Result;
@@ -64,30 +65,27 @@ pub enum SpecialPath {
 #[tauri::command]
 #[specta::specta]
 pub fn open_special_path(special_path: SpecialPath) -> CommandResult<()> {
-    let open_result = |path: Result<PathBuf>| {
-        let path = path
-            .tap_err(|e| log::error!("Couldn't determine path for {:?}: {e}", special_path))
-            .map_err(CommandError::from)?;
-        if !path.exists() {
-            Err(CommandError::from(
-                format!("Path doesn't exist: {:?}", path).tap(|s| log::error!("{s}")),
-            ))?
-        }
-        open_path(
-            path.to_str()
-                .ok_or("Path couldn't be converted to string.".to_string())
-                .tap_err(|e| log::error!("{e}"))
-                .map_err(CommandError::from)?,
-        )
-        .tap_err(|e| log::error!("Couldn't open path: {e}"))
-        .map_err(CommandError::from)
+    let map_result = |path: Result<PathBuf>| {
+        path.tap_err(|e| log::error!("Couldn't determine path for {:?}: {e}", special_path))
+            .map_err(CommandError::from)
     };
 
-    let open_option = |path: Option<PathBuf>| {
-        let path = path
-            .ok_or(format!("Couldn't determine path for {:?}", special_path))
+    let map_option = |path: Option<PathBuf>| {
+        path.ok_or(format!("Couldn't determine path for {:?}", special_path))
             .tap_err(|e| log::error!("{e}"))
-            .map_err(CommandError::from)?;
+            .map_err(CommandError::from)
+    };
+
+    let create_it = |path: PathBuf| {
+        if !path.exists() {
+            fs::create_dir_all(&path)
+                .tap_err(|e| log::error!("Couldn't create folder {path:?}: {e}"))
+                .map_err(CommandError::from)?;
+        }
+        Ok(path)
+    };
+
+    let open_it = |path: PathBuf| {
         if !path.exists() {
             Err(CommandError::from(
                 format!("Path doesn't exist: {:?}", path).tap(|s| log::error!("{s}")),
@@ -104,16 +102,19 @@ pub fn open_special_path(special_path: SpecialPath) -> CommandResult<()> {
     };
 
     match special_path {
-        SpecialPath::AppInstallFolder => open_result(get_install_path()),
-        SpecialPath::AppConfigFolder => open_option(get_config_path()),
-        SpecialPath::AppTranslationsFolder => open_option(get_translation_folder_path()),
-        SpecialPath::SteamScreenshotFolder => open_option(
+        SpecialPath::AppInstallFolder => map_result(get_install_path()).and_then(open_it),
+        SpecialPath::AppConfigFolder => map_option(get_config_path()).and_then(open_it),
+        SpecialPath::AppTranslationsFolder => map_option(get_translation_folder_path())
+            .and_then(create_it)
+            .and_then(open_it),
+        SpecialPath::SteamScreenshotFolder => map_option(
             // TODO: Open multiple? Handle errors?
             find_steam_installation_folders()
                 .ok()
                 .and_then(|steam_paths| steam_paths.first().cloned())
                 .and_then(|steam_path| get_steam_screenshots_folders(steam_path).ok())
                 .and_then(|screenshot_paths| screenshot_paths.first().cloned()),
-        ),
+        )
+        .and_then(open_it),
     }
 }
